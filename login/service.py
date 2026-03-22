5e80c53a2c52acb2ddea9dadd5b315615f8f7da4
leonardo almeida <leonardoalmeida196@outlook.com>
2026-03-22 16:12:15 -0300

    fix: fix logout method call in login/service.py

--- login/service.py
+++ login/service.py
@@ -16,6 +16,6 @@ def login(username, password):
 
 
 def logout():
-    for key in st.session_state.key():
+    for key in st.session_state.keys():
         del st.session_state[key]
     st.rerun()
